3754a4e48fd9bae56dcaa0288959077023bac343
wjt <wangjiantaoafei@163.com>
2021-02-07 14:59:39 +0800

    完善 UI测试 并提交编译好的dll 可支持在cad 2020 x64上直接测试

--- Demo/UIDemo/Arx_Net/NetFormDll/NetFormDll/CppAPI/CppFunFactory.cs
+++ Demo/UIDemo/Arx_Net/NetFormDll/NetFormDll/CppAPI/CppFunFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +9,22 @@ namespace NetFormDll.CppAPI
 {
     public class CppFunFactory
     {
-        static private string DllPath = @"E:\CvsWork\JietuPublic\Arx_Net\ArxProjectUserNetExcel\x64\Release_v141\ArxProjectUserNetExcel.arx";
+        static private string DllPath = @"ArxProjectOutCFun.arx";
         static private ICppFunctions g_CppFun = null;
+
+        static string GetBinPath()
+        {
+            string strLocatios = Assembly.GetExecutingAssembly().Location;
+            string strDllPath = System.IO.Path.GetDirectoryName(strLocatios);
+//             System.IO.DirectoryInfo dirInfo = new System.IO.DirectoryInfo(strDllPath);
+//             string strBin = dirInfo.Parent.FullName + @"\";
+            return strDllPath;
+        }
+
         static public ICppFunctions GetSingle()
         {
             if (null == g_CppFun)
-                g_CppFun = new CppFunctions(DllPath);
+                g_CppFun = new CppFunctions(System.IO.Path.Combine(GetBinPath(), DllPath));
 
             return g_CppFun;
         }
